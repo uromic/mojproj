f08a81318454e7aadafcb93f397e0f50ce362b58
uros <uros@urosRC>
2012-11-21 21:38:47 +0100

    commit1

---
+++ mojproj/src/main/java/com/uromic/mojproj/util/MyUtil.java
@@ -0,0 +1,13 @@
+package com.uromic.mojproj.util;
+
+public class MyUtil {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
